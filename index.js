2fd10b3b246e8c7d3fad70e1f6554e283513e805
Lira11io <Lira11pm@mail.ru>
2023-04-22 00:16:52 +0300

    add files and theory answers

---
+++ index.js
@@ -0,0 +1,33 @@
+//Решение 4 вопроса
+let elemValue = document.querySelectorAll("input"); //получаем все элементы input
+console.log(elemValue[0].value); //выводим значение текстового элемента в консоль
+
+//Решение 5 вопроса
+//let mas = new Array(2);
+//console.log(mas);
+//console.log(mas.length);
+//mas[3] = 5;
+//console.log(mas);
+//console.log(mas[3]);
+
+//Решение 6 вопроса
+//let mas = new Array(2);
+//mas[3] = 5;
+//console.log(mas3);
+
+//Решение 9 вопроса
+//let a = new Array(2);
+//a[1] = null;
+//console.log(a);
+//console.log(a.length);
+
+//Решение 10 вопроса
+/* const students = [“Lena", “Olya", "Ylia", "Roma" ,  "Vova" ];
+console.log(students[3]); */
+
+//Решение 11 вопроса
+const fruits = ["Груша", "Яблоко", "Лимон", "Ананас"];
+
+console.log(fruits.shift());
+
+console.log(fruits);
